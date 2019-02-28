385014f09585553f9e0b34bfc54216a7acca9e07
rsobrepe <raymon.sobrepena@ryerson.ca>
2019-02-28 01:10:39 -0500

    Initial Commit

---
+++ app/src/main/java/app/ray/com/myshift/beginActivity.java
@@ -0,0 +1,13 @@
+package app.ray.com.myshift;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class beginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_begin);
+    }
+}
